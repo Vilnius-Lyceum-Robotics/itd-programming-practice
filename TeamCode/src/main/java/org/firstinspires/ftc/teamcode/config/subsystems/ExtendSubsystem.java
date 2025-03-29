366c4c1454d36fdded5ef239f8efe1a1b9d39b2c
Aistis <aistis.verkauskas@gmail.com>
2025-03-29 19:21:54 +0200

    Removed states enum because no servos are being used here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/ExtendSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/ExtendSubsystem.java
@@ -2,25 +2,17 @@ package org.firstinspires.ftc.teamcode.config.subsystems;
 
 import static org.firstinspires.ftc.teamcode.config.core.RobotConstants.*;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.controller.PIDController;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
-import com.seattlesolvers.solverslib.solversHardware.SolversMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class ExtendSubsystem extends SubsystemBase {
 
-    public enum ExtendState {
-        ZERO, FULL, TRANSFER
-    }
-
     private Telemetry telemetry;
     private Motor extendMotor;
-    private ExtendState extendState = ExtendState.ZERO;
     private PIDController pid;
     private boolean pidOn = false;
     private int target;
@@ -41,7 +33,7 @@ public class ExtendSubsystem extends SubsystemBase {
     @Override
     public void periodic(){
         if(!pidOn) {
-            extendMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+//            extendMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
             return;
         }
         if(reachedTarget()) {
@@ -50,29 +42,15 @@ public class ExtendSubsystem extends SubsystemBase {
         }
 
         pid.setPID(EXTEND_kP, EXTEND_kI, EXTEND_kD);
-        extendMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+//        extendMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
         double encoderPosition = getPos();
         double power = pid.calculate(encoderPosition);
-//            if(encoderPosition < 50 && target < 50) extendMotor.setPower(0); // Copied from Indubitables
         extendMotor.set(power);
     }
 
-    public void setTarget(ExtendState state){
-        extendState = state;
+    public void setTarget(int target){
         pidOn = true;
-
-        switch(state) {
-            case FULL:
-                target = EXTEND_FULL;
-                break;
-            case ZERO:
-                target = EXTEND_ZERO;
-                break;
-            case TRANSFER:
-                target = EXTEND_TRANSFER;
-                break;
-        }
-
+        this.target = target;
         pid.setSetPoint(target);
     }
 
@@ -80,28 +58,24 @@ public class ExtendSubsystem extends SubsystemBase {
         return extendMotor.getCurrentPosition();
     }
 
-    public ExtendState getState(){
-        return extendState;
-    }
-
     public boolean reachedTarget(){
         return pid.atSetPoint(); // Math.abs(errorVal_p) < errorTolerance_p
     }
 
     public void toFull(){
-        setTarget(ExtendState.FULL);
+        setTarget(EXTEND_FULL);
     }
 
     public void toZero(){
-        setTarget(ExtendState.ZERO);
+        setTarget(EXTEND_ZERO);
     }
 
     public void toTransfer(){
-        setTarget(ExtendState.TRANSFER);
+        setTarget(EXTEND_TRANSFER);
     }
 
     public void telemetry(){
         telemetry.addData("Extend pos: ", getPos());
-        telemetry.addData("Target pos: ", target);
+        telemetry.addData("Extend target pos: ", target);
     }
 }
