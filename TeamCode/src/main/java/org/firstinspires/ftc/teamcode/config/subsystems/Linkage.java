c4d26040f94529c4d2285d7ff7592b070934f2e9
bulviakas <misteridiot2004@gmail.com>
2025-04-30 12:01:02 +0300

    New linkage subsystem and it's test opmode adaptation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Linkage.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.config.subsystems;
+
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import static org.firstinspires.ftc.teamcode.config.core.RobotConstants.*;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Linkage extends SubsystemBase {
+    private Telemetry telemetry;
+    private Servo leftLinkage;
+    private Servo rightLinkage;
+    private double pos = 0.1;
+
+    public Linkage(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+
+        leftLinkage = hardwareMap.get(Servo.class, "leftLinkage");
+        rightLinkage = hardwareMap.get(Servo.class, "rightLinkage");
+        leftLinkage.setDirection(Servo.Direction.FORWARD);
+        rightLinkage.setDirection(Servo.Direction.REVERSE);
+
+        setTarget(this.pos);
+    }
+
+    public void setTarget(double target) {
+        if (target >= H_ARM_FULL) {
+            this.pos = H_ARM_FULL;
+            leftLinkage.setPosition(H_ARM_FULL);
+            rightLinkage.setPosition(H_ARM_FULL);
+        } else if (target <= H_ARM_FORCE) {
+            this.pos = H_ARM_FORCE;
+            leftLinkage.setPosition(H_ARM_FORCE);
+            rightLinkage.setPosition(H_ARM_FORCE);
+        } else {
+            leftLinkage.setPosition(target);
+            rightLinkage.setPosition(target);
+        }
+    }
+
+    public double getPos() {
+        return this.pos;
+    }
+
+    public void telemetry() {
+        telemetry.addData("Linkage servo position: ", getPos());
+    }
+    public void setMappedTarget(Double input){
+        double clippedInput = Range.clip(input, -1, 1);
+        double mappedPos = Range.scale(clippedInput, -1, 1, LINKAGE_ZERO, LINKAGE_FULL);
+        setTarget(mappedPos);
+    }
+
+    public void increment(double increment) {
+        setTarget(this.pos + increment);
+    }
+    public void extend() {
+        setTarget(H_ARM_FULL);
+    }
+    public void retract() {
+        setTarget(H_ARM_DEFAULT);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/LinkageTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/LinkageTest.java
@@ -2,18 +2,19 @@ package org.firstinspires.ftc.teamcode.opmode.tests;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.RunCommand;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
-import org.firstinspires.ftc.teamcode.config.subsystems.LinkageSubsystem;
-
+import org.firstinspires.ftc.teamcode.config.subsystems.Linkage;
+@TeleOp(name = "notSoSadLinkageTest", group = "!")
 public class LinkageTest extends CommandOpMode {
 
-    private LinkageSubsystem linkageSubsystem;
-    private GamepadEx operator;
+    private Linkage linkage;
+    private GamepadEx firstDriver;
 
     @Override
     public void initialize(){
@@ -22,17 +23,26 @@ public class LinkageTest extends CommandOpMode {
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        linkageSubsystem = new LinkageSubsystem(hardwareMap, telemetry);
+        linkage = new Linkage(hardwareMap, telemetry);
 
-        operator = new GamepadEx(gamepad1);
+        firstDriver = new GamepadEx(gamepad1);
 
-        operator.getGamepadButton(GamepadKeys.Button.DPAD_UP)
-                .whenPressed(new InstantCommand(linkageSubsystem::toFull));
+        firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_UP)
+                .whenPressed(() -> linkage.extend());
 
-        operator.getGamepadButton(GamepadKeys.Button.DPAD_DOWN)
-                .whenPressed(new InstantCommand(linkageSubsystem::toZero));
+        firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_DOWN)
+                .whenPressed(() -> linkage.retract());
 
-        operator.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT)
+        firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT)
+                .whenPressed(() -> linkage.increment(0.01));
+        firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_LEFT)
+                .whenPressed(() -> linkage.increment(-0.01));
+
+        //firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(new InstantCommand(linkageSubsystem::toFull));
+
+        //firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(new InstantCommand(linkageSubsystem::toZero));
+
+        /*firstDriver.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT)
                 .toggleWhenPressed(
                         new RunCommand(
                             () -> linkageSubsystem.setMappedTarget(operator.getLeftY()),
@@ -42,14 +52,14 @@ public class LinkageTest extends CommandOpMode {
 //        linkageSubsystem.setDefaultCommand(new RunCommand(
 //                () -> linkageSubsystem.setMappedTarget(operator.getLeftY()),
 //                linkageSubsystem
-//        ));
+//        ));*/
     }
 
     @Override
     public void run(){
         super.run(); // DO NOT REMOVE! Runs FTCLib Command Scheduler
 
-        linkageSubsystem.telemetry();
+        linkage.telemetry();
 
         telemetry.update(); // DO NOT REMOVE! Needed for telemetry
     }
