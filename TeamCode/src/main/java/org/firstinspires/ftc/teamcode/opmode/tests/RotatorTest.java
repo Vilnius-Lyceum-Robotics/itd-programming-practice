82f780031a811c7db7c24a9e79af2a1c5d0c857e
FaustasSur <surplys.faustas@gmail.com>
2025-09-05 13:53:22 +0300

    Rotator test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/RotatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/RotatorTest.java
@@ -16,8 +16,6 @@ public class RotatorTest extends CommandOpMode {
     private GamepadEx firstDriver;
     private static final double ROTATOR_TURNS_PER_SEC = 0.5;
     private static final double ANGLE_TURNS_PER_SEC = 0.5;
-    private int rotatorState = 0;
-
     @Override
     public void initialize() {
         super.reset();
@@ -26,18 +24,6 @@ public class RotatorTest extends CommandOpMode {
         rotator = new Rotator(hardwareMap, telemetry);
         firstDriver = new GamepadEx(gamepad1);
 
-        firstDriver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
-                .whenPressed(() -> {
-                    if(rotatorState == 0) rotatorState = 1;
-                }).whenReleased(() -> {
-                    if(rotatorState == 1) rotatorState = 0;
-                });
-        firstDriver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER)
-                .whenPressed(() -> {
-                    if(rotatorState == 0) rotatorState = -1;
-                }).whenReleased(() -> {
-                    if(rotatorState == -1) rotatorState = 0;
-                });
         firstDriver.getGamepadButton(GamepadKeys.Button.TRIANGLE)
                 .whenPressed(() -> rotator.setAngle(IN_ANGLE_UP));
         firstDriver.getGamepadButton(GamepadKeys.Button.CROSS)
@@ -53,6 +39,7 @@ public class RotatorTest extends CommandOpMode {
         lastTick = System.nanoTime();
 
         double angleState = firstDriver.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) - firstDriver.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER);
+        int rotatorState = (firstDriver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).get() ? 1 : 0) - (firstDriver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).get() ? 1 : 0);
 
         rotator.setRotatorRel(mult * ROTATOR_TURNS_PER_SEC * rotatorState);
         rotator.setAngleRel(mult * ANGLE_TURNS_PER_SEC * angleState);
